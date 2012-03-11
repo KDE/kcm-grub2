218d815ad8c567f1a37a9ca9b086b5a934eb3c4b
Konstantinos Smanis <konstantinos.smanis@gmail.com>
2012-03-11 23:37:02 +0200

    Fix previous commit.

--- src/helper/helper.cpp
+++ src/helper/helper.cpp
@@ -119,7 +119,11 @@ ActionReply Helper::install(QVariantMap args)
     }
 
     KProcess grub_install;
-    grub_install.setShellCommand(QString("%1 --root-directory=%2 %3").arg(installExePath, KShell::quoteArg(mountPoint), KShell::quoteArg(mbrInstall ? partition.remove(QRegExp("\\d+")) : "--force " + partition)));
+    if (mbrInstall) {
+        grub_install.setShellCommand(QString("%1 --root-directory=%2 %3").arg(installExePath, KShell::quoteArg(mountPoint), KShell::quoteArg(partition.remove(QRegExp("\\d+")))));
+    } else {
+        grub_install.setShellCommand(QString("%1 --root-directory=%2 --force %3").arg(installExePath, KShell::quoteArg(mountPoint), KShell::quoteArg(partition)));
+    }
     grub_install.setOutputChannelMode(KProcess::MergedChannels);
     if (grub_install.execute() != 0) {
         reply = ActionReply::HelperErrorReply;
